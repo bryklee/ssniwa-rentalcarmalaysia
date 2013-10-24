d38023c1604035b15d3591aeeadf17ef719d01cf
Ming <bryklee@gmail.com>
2013-10-24 09:51:49 +0800

    Added Email functionality

--- src/main/java/my/com/rentalcarmalaysia/servlet/ContactUsServlet.java
+++ src/main/java/my/com/rentalcarmalaysia/servlet/ContactUsServlet.java
@@ -1,12 +1,22 @@
 package my.com.rentalcarmalaysia.servlet;
 
 import java.io.IOException;
+import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import my.com.rentalcarmalaysia.util.Base64;
 import net.tanesha.recaptcha.ReCaptchaImpl;
 import net.tanesha.recaptcha.ReCaptchaResponse;
 
@@ -21,28 +31,30 @@ public class ContactUsServlet extends HttpServlet {
     public static final String RESP_RESULT_ERR_VERIFY_CAPTCHA = "-2";
     
     private String recaptcha_privatekey;
+    private String smtp_pwd;
     
     @Override
     public void init() throws ServletException {
         super.init();
-        recaptcha_privatekey = System.getenv("RECAPTCHA_PRI");
+        recaptcha_privatekey = System.getenv("RECAPTCHA_PRI") == null ? "" : System.getenv("RECAPTCHA_PRI").trim();
+        smtp_pwd = System.getenv("SMTP_PWD") == null ? "" : System.getenv("SMTP_PWD").trim();
+        smtp_pwd = smtp_pwd.length() > 0 ? new String(Base64.decode(smtp_pwd.getBytes())) : smtp_pwd;
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         JSONObject jsonobj = new JSONObject();
-        if( recaptcha_privatekey == null || recaptcha_privatekey.trim().equals("") ) {
+        if( recaptcha_privatekey.equals("") || smtp_pwd.equals("") ) {
             putJsonobj(jsonobj, RESP_FIELD_CODE, RESP_RESULT_ERR_UNKNOWN);
             commitJSONResp(resp, jsonobj);
             return;
         }
         
-        String remoteAddr = req.getRemoteAddr();
-        String challenge = req.getParameter("captchaChall");
-        String uresponse = req.getParameter("captchaResp");
+        String remoteAddr = req.getRemoteAddr() == null ? "" : req.getRemoteAddr().trim();
+        String challenge = req.getParameter("captchaChall") == null ? "" : req.getParameter("captchaChall").trim();
+        String uresponse = req.getParameter("captchaResp") == null ? "" : req.getParameter("captchaResp").trim();
         
-        if( remoteAddr == null || remoteAddr.trim().equals("")
-                || challenge == null || challenge.trim().equals("")) {
+        if( remoteAddr.equals("") || challenge.equals("")) {
             putJsonobj(jsonobj, RESP_FIELD_CODE, RESP_RESULT_ERR_UNKNOWN);
             commitJSONResp(resp, jsonobj);
             return;
@@ -58,7 +70,9 @@ public class ContactUsServlet extends HttpServlet {
             return;
         }
         
-        //TODO: Send email
+        if( !sendContactUsEmail(req, resp, jsonobj) ) {
+            return;
+        }
         
         putJsonobj(jsonobj, RESP_FIELD_CODE, RESP_RESULT_SUCCESS);
         commitJSONResp(resp, jsonobj);
@@ -76,6 +90,72 @@ public class ContactUsServlet extends HttpServlet {
     @SuppressWarnings("unchecked")
     private Object putJsonobj(JSONObject jsonobj, String key, String value) {
         return jsonobj.put(key, value);
+    }
+    
+    private boolean sendContactUsEmail(HttpServletRequest req, HttpServletResponse resp, JSONObject jsonobj) throws IOException {
+        String contactName = req.getParameter("contactName") == null ? "" : req.getParameter("contactName").trim();
+        String contactEmail = req.getParameter("contactEmail") == null ? "" : req.getParameter("contactEmail").trim();
+        String contactPhone = req.getParameter("contactPhone") == null ? "" : req.getParameter("contactPhone").trim();
+        String contactMessage = req.getParameter("contactMessage") == null ? "" : req.getParameter("contactMessage").trim();
+        
+        if( contactEmail.equals("") || contactMessage.equals("")) {
+            putJsonobj(jsonobj, RESP_FIELD_CODE, RESP_RESULT_ERR_UNKNOWN);
+            commitJSONResp(resp, jsonobj);
+            return false;
+        }
+        
+        contactPhone = contactPhone.equals("") ? "-" : contactPhone;
+        
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        
+        try {
+            Session session = Session.getDefaultInstance(props,
+                    new Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication("ssniwalee@gmail.com", smtp_pwd);
+                        }
+                    }
+            );
+            
+            InternetAddress[] replyToAdd = new InternetAddress[1];
+            replyToAdd[0] = new InternetAddress(contactEmail, contactName);
+            
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("ssniwalee@gmail.com", "SSNiwa Mailer"));
+            message.setReplyTo(replyToAdd);
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("bryklee@gmail.com"));
+            message.setSubject("Enquiry from www.rentalcarmalaysia.com.my contact-us form");
+            
+            StringBuilder sb = new StringBuilder();
+            sb.append("<p><b>Name:</b> ");
+            sb.append(contactName);
+            sb.append("</p>");
+            sb.append("<p><b>Email:</b> ");
+            sb.append(contactEmail);
+            sb.append("</p>");
+            sb.append("<p><b>Phone:</b> ");
+            sb.append(contactPhone);
+            sb.append("</p>");
+            sb.append("<p><b>Message:</b><br/>");
+            sb.append(contactMessage);
+            sb.append("</p>");
+            message.setContent(sb.toString(), "text/html" );
+    
+            Transport.send(message);
+        }
+        catch(MessagingException me) {
+            putJsonobj(jsonobj, RESP_FIELD_CODE, RESP_RESULT_ERR_UNKNOWN);
+            commitJSONResp(resp, jsonobj);
+            return false;
+        }
+        
+        return true;
     }
     
 }
